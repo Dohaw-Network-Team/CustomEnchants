27a23414d60176025e1225bb50789baa35c7f3cc
Skiller9090 <37740659+Skiller9090@users.noreply.github.com>
2020-04-17 14:21:41 +0100

    logic rework

--- src/me/c10coding/CustomEnchants/enchants/SwiftFootEnchant.java
+++ src/me/c10coding/CustomEnchants/enchants/SwiftFootEnchant.java
@@ -5,24 +5,21 @@ import me.caleb.Classes.Main;
 import me.caleb.Classes.utils.managers.AttributeManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.enchantments.EnchantmentTarget;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-import javax.management.Attribute;
+import java.util.Objects;
+
 
 public class SwiftFootEnchant extends CustomEnchant{
 
     public SwiftFootEnchant(){
-        super(EnchantmentKeys.swift_foot, ChatColor.GOLD, Particle.FLASH);
+        super(EnchantmentKeys.swift_foot, ChatColor.DARK_GRAY, Particle.FLASH);
         Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
@@ -30,36 +27,22 @@ public class SwiftFootEnchant extends CustomEnchant{
     public void onInventoryClick(InventoryClickEvent e){
 
         Player p = (Player) e.getWhoClicked();
-        Inventory inv = p.getInventory();
-        InventoryAction action = e.getAction();
 
 
         if(e.getClickedInventory() == null) return;
 
         if(e.getCurrentItem() == null) return;
 
-        if(e.getSlotType().equals(InventoryType.SlotType.ARMOR) && e.getCurrentItem().getType().name().toLowerCase().contains("boots")){
-           if(action.equals(InventoryAction.PICKUP_ALL) || action.equals(InventoryAction.PLACE_ALL) || action.equals(InventoryAction.MOVE_TO_OTHER_INVENTORY)){
-                if(e.getCurrentItem().getType().name().toLowerCase().contains("boots") || e.getCursor().getType().name().toLowerCase().contains("boots")){
-                    ItemStack boots = e.getCurrentItem().getType().name().toLowerCase().contains("boots") ? e.getCurrentItem() : e.getCursor();
-                    if (boots.getItemMeta() != null){
-                        if(boots.getItemMeta().hasEnchant(this)) {
-                            AttributeManager am = new AttributeManager(Main.getInstance(), p);
-                            if (action.equals(InventoryAction.PICKUP_ALL)) {
-                                am.reApplyClassSpeedModifier();
-                            } else if (action.equals(InventoryAction.MOVE_TO_OTHER_INVENTORY)) {
-                                if (p.getInventory().getBoots() == null) {
-                                    am.setSpeedModifiers(boots.getItemMeta().getEnchantLevel(this));
-                                } else {
-                                    am.reApplyClassSpeedModifier();
-                                }
-                            } else if (action.equals(InventoryAction.PLACE_ALL)) {
-                                am.setSpeedModifiers(boots.getItemMeta().getEnchantLevel(this));
-                            }
-                        }
-                    }
+        ItemStack playerBoots = p.getInventory().getBoots();
+        AttributeManager am = new AttributeManager(Main.getInstance(), p);
+        if (e.getSlotType().equals(InventoryType.SlotType.ARMOR)){
+            if (playerBoots != null){
+                if (Objects.requireNonNull(playerBoots.getItemMeta()).hasEnchant(this)){
+                    am.reApplyClassSpeedModifier();
                 }
-           }
+            }else if (playerBoots == null || !(playerBoots.getItemMeta().hasEnchant(this))){
+                am.setSpeedModifiers(playerBoots.getItemMeta().getEnchantLevel(this));
+            }
         }
     }
 
